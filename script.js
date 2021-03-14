7e1713deed2338e4e9098ebd71e2c5e74621cc79
FyodorZanegin <fyodor.zanegin@gmail.com>
2021-03-14 17:09:58 +0300

    new

--- script.js
+++ script.js
@@ -60,10 +60,28 @@ $(document).ready(function () {
 
 
 
-// Form
-
-
 document.addEventListener("DOMContentLoaded", function () {
+
+
+
+  // Burger
+
+
+  header__burger.onclick = function (e) {
+
+    this.classList.toggle("active");
+
+    header__menu.classList.toggle("active");
+
+    document.body.classList.toggle("lock");
+
+  };
+
+
+
+  // Form
+
+
   const form = document.getElementById("form");
 
   form.addEventListener("submit", formSend);
@@ -198,7 +216,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     element.innerHTML = 500 - this.value.length;
 
-    if (500 - this.value.length <= 0) {
+    if (500 - this.value.length <= 50) {
       element.style.color = '#BF3330';
 
     } else {
@@ -209,50 +227,122 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-  // Burger
-
-
-  header__burger.onclick = function (e) {
-
-    this.classList.toggle("active");
-
-    header__menu.classList.toggle("active");
-
-    document.body.classList.toggle("lock");
-
-  };
-
-
-
   // Phone Mask
 
 
-  phone.onfocus = phone.oninput = function (e) {
+  const mask = "+7 (___) ___-__-__",
+    input = document.getElementsByClassName('phone-input');
 
-    const template = '+7 (___) ___-__-__',
-      def = template.replace(/\D/g, ""),
-      val = this.value.replace(/\D/g, "");
-    let i = 0,
 
-      newValue = template.replace(/[_\d]/g, function (a) {
-        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-      });
+  for (var i = 0; i < input.length; i++) {
 
-    i = newValue.indexOf("_");
+    input[i].addEventListener('focus', function (e) {
 
-    if (i !== -1) {
-      newValue = newValue.slice(0, i);
+      this.autocomplete = "off"
+
+      if (mask.length > this.value.length) {
+        for (var i = 0; i < mask.length; i++) {
+
+          if (mask[i] == '_') {
+            e.preventDefault()
+            i_mask = save_mask = i
+            let th = this;
+            window.setTimeout(function () {
+              th.value = mask
+              th.setSelectionRange(i_mask, i_mask);
+            }, 1);
+            return true
+          }
+
+        }
+      }
+    })
+
+
+    input[i].addEventListener("blur", function () {
+      for (var i = 0; i < mask.length; i++) {
+        this.value[i] == '_' ? this.value = '' : false
+      }
+    });
+
+
+    input[i].addEventListener('click', function () {
+      (this.selectionStart >= save_mask) ? i_mask = this.selectionStart : false
+    })
+
+
+    if (window.screen.width >= 1200) {
+      input[i].addEventListener('input', function (e) {
+        let th = this
+        step(th)
+      })
     }
 
-    this.value = newValue;
+    input[i].addEventListener('keydown', function (e) {
+      let th = this,
+        mask_number = true,
+        arrow = false;
 
-    console.log(newValue);
-  }
+      if (e.keyCode == 37) {
+        e.preventDefault()
+        if (save_mask < i_mask) i_mask--
+        th.setSelectionRange(i_mask, i_mask);
+        mask_number = false
+        arrow = true
+      }
+      else if (e.keyCode == 39 && mask.length > i_mask) {
+        e.preventDefault()
+        arrow = true
+        i_mask++
+      }
+      else if (e.keyCode == 8) {
+        e.preventDefault()
+        window.setTimeout(function () {
+          if (i_mask == save_mask && window.screen.width >= 1200) {
+            th.value = mask
+          }
+          else if (i_mask == save_mask && window.screen.width <= 1200) {
+            th.value = ''
+            th.blur()
+            return false
+          }
+          else {
+            i_mask--
+          }
+          let mask_number = false
+          step_input(mask_number, th, arrow)
+          return false
+        }, 1);
+      }
+      else if (window.screen.width <= 1200) {
+        window.setTimeout(function () {
+          step(th)
+        }, 1);
+      }
 
-  phone.onblur = function (e) {
-    if (this.value.length < 18) {
-      this.value = "";
+      step_input(mask_number, th, arrow)
+    })
+
+    function step(th) {
+      if (+th.value[i_mask] && mask.length > i_mask || th.value[i_mask] == '0' && mask.length > i_mask) {
+        i_mask++
+      }
+      let mask_number = true,
+        arrow = false
+      step_input(mask_number, th, arrow)
     }
+
+
+    function step_input(mask_number, th, arrow) {
+      if (arrow == false) th.value = th.value.substr(0, i_mask) + mask.substr(i_mask, mask.length)
+      th.setSelectionRange(i_mask, i_mask);
+
+      if (mask[i_mask] != '_' && mask.length > i_mask) {
+        (mask_number == true) ? i_mask++ : i_mask--
+        step_input(mask_number, th, arrow)
+      }
+    }
+
   }
 
 
